667028f260f507ce40ac0db07295342cf1216323
Ryan Kelley <kelleyrw@gmail.com>
2024-04-17 12:49:23 -0700

    first iteration

--- test_filtering_interface_poc.py
+++ test_filtering_interface_poc.py
@@ -1,20 +1,29 @@
-import json
+from abc import ABC, abstractmethod
 from enum import Enum
-
-import pytest
 
 
 class Operator(Enum):
     OR: str = "or"
     AND: str = "and"
     NOT: str = "not"
-    EQUAL: str = "eq"
+    EQUAL: str = "="
 
 
-class Expression:
+class Expression(ABC):
 
+    @abstractmethod
     def evaluate(self) -> str:
         pass
+
+
+class BinaryOperatorExpression:
+    def __init__(self, lhs: Expression, op: Operator, rhs: Expression):
+        self.lhs = lhs
+        self.op = op
+        self.rhs = rhs
+
+    def evaluate(self) -> str:
+        return f"{self.lhs.evaluate()} {self.op} {self.rhs.evaluate()}"
 
 
 class Literal(Expression):
@@ -25,22 +34,8 @@ class Literal(Expression):
         return self.value
 
 
-class EqualExpression(Expression):
-    def __init__(self, lhs: Expression, rhs: Expression):
-        self.lhs = lhs
-        self.rhs = rhs
-
-    def evaluate(self) -> str:
-        return f"{self.lhs.evaluate()} = {self.rhs.evaluate()}"
-
-
-class ORExpression(Expression):
-    def __init__(self, lhs: Expression, rhs: Expression):
-        self.lhs = lhs
-        self.rhs = rhs
-
-    def evaluate(self) -> str:
-        return f"{self.lhs.evaluate()} or {self.rhs.evaluate()}"
+def LogicalEqual(lhs: Expression, rhs: Expression) -> BinaryOperatorExpression:
+    return BinaryOperatorExpression(lhs, Operator.EQUAL.value, rhs)
 
 
 def test_Literal():
@@ -52,41 +47,33 @@ def test_EqualExpression(subtests):
     v1 = Literal(7)
     f1 = Literal("A")
     with subtests.test(msg="A = 7"):
-        eq1 = EqualExpression(f1, v1)
+        eq1 = LogicalEqual(f1, v1)
         assert eq1.evaluate() == "A = 7"
     with subtests.test(msg="7 = A"):
-        eq1 = EqualExpression(v1, f1)
+        eq1 = LogicalEqual(v1, f1)
         assert eq1.evaluate() == "7 = A"
     with subtests.test(msg="A = A"):
-        eq1 = EqualExpression(f1, f1)
+        eq1 = LogicalEqual(f1, f1)
         assert eq1.evaluate() == "A = A"
     with subtests.test(msg="7 = 7"):
-        eq1 = EqualExpression(v1, v1)
+        eq1 = LogicalEqual(v1, v1)
         assert eq1.evaluate() == "7 = 7"
 
     with subtests.test(msg="7 = 7"):
         f2 = Literal("B")
         v2 = Literal("42")
-        eq1 = EqualExpression(f1, v1)
-        eq2 = EqualExpression(f2, v2)
-        compound = ORExpression(eq1, eq2)
-        assert compound.evaluate() == "A = 7 or B = 42"
+        eq1 = LogicalEqual(f1, v1)
+        eq2 = LogicalEqual(f2, v2)
+        compound = LogicalEqual(eq1, eq2)
+        assert compound.evaluate() == "A = 7 = B = 42"
 
 
-# class Filter:
-#     def __init__(self, expression: Expression):
-#         self.expression = expression
+# @pytest.fixture()
+# def trival():
+#     value = dict(operator="=", field="A", value="7")
+#     return value
 #
-#     def evaluate(self) -> str:
-#         self.exp
-
-
-@pytest.fixture()
-def trival():
-    value = dict(operator="=", field="A", value="7")
-    return value
-
-
-def test_foo(trival):
-    print(trival)
-    assert True
+#
+# def test_foo(trival):
+#     print(trival)
+#     assert True
